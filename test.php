e3f7c1e4615349688bcce7effe57499144d2e446
Reda3022 <99341151+Reda3022@users.noreply.github.com>
2022-09-25 19:13:31 +0200

    asdada

--- test.php
+++ test.php
@@ -1,5 +1,14 @@
 <?php
-$con=mysqli_connect('localhost','root','','projet_reda');
+//
+
+$conn=mysqli_connect('localhost','root','','projet_reda');
+
+$sql = "SELECT * FROM tb_localite";
+$result = $conn->prepare($sql);
+
+//exce
+$result->execute();
+
 
 ?>
 <!doctype html>
@@ -35,18 +44,58 @@ $con=mysqli_connect('localhost','root','','projet_reda');
 
     <label for="Localite">Localité</label>
     <select name="loc" id="loc">
-        <?php $reponse = $con->query('SELECT * FROM "tb_localite"');
-        while ($donnees = $reponse->fetch())
-        {
-            ?>
-            <option value="<?php echo $donnees['id_loc']; ?>">
-                <?php echo $donnees['npa_loc']; ?>
-            </option>
-        <?php } ?>
-    </select>
+        <libellé>NPA</libellé>
+        <?php
 
+        try {
+            foreach ($result->fetch() as $row ) {
+                echo "<option value='".$row['npa_loc']."'>".$row['nom_loc']."</option>";
+            }
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+        ?>
+    </select>
+    <br></br>
     <input type="submit" value="Ajouter">
 
 	</form>
+
+<!-- //*********************MODIFICATION*********************// -->
+<!-- <form action="connection.php" method="POST">
+    <h1>Modification d'un client:</h1>
+
+    <label for="idClient">
+        Entrez l'id du client que vous voulez modifier:
+        <input type="text" name="idClient" id="idClient">
+    </label><br><br>
+
+    <hr style="border-bottom: 2px solid black; margin-bottom: 30px;">
+
+    <label for="nomC">
+        Entrez le nouveau nom:
+        <input type="text" name="nomC" id="nomC">
+    </label><br><br>
+    <label for="numTel">
+        Entrez le nouveau numero de tel:
+        <input type="text" name="numTel" id="numTel">
+    </label><br><br>
+    <label for="adresse">
+        Entrez la nouvelle adresse:
+        <input type="text" name="adresse" id="adresse">
+    </label><br><br>
+    <input type="submit" value="Modifier">
+
+</form> -->
+<!-- //*********************SUPPRESSION*********************// -->
+<!-- <form action="connection.php" method="POST">
+    <h1>Suppression d'un client:</h1>
+    <label for="idClient">
+        Entrez l'id du client que vous voulez supprimer:
+        <input type="text" name="idClient" id="idClient">
+    </label>
+    <input type="submit">
+
+</form> -->
 </body>
 </html>
